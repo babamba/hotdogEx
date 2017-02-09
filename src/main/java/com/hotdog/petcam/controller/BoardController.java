5957ee771e563f7e45db65ecee1107329dc6fc9a
bit-user <bit-user@bit>
2017-02-09 13:54:10 +0900

    dd

--- src/main/java/com/hotdog/petcam/controller/BoardController.java
+++ src/main/java/com/hotdog/petcam/controller/BoardController.java
@@ -1,8 +1,15 @@
 package com.hotdog.petcam.controller;
 
-import com.hotdog.petcam.vo.BoardVo;
+import org.springframework.stereotype.Controller;
 
+import com.hotdog.petcam.vo.BoardVo;
+import com.hotdog.petcam.vo.UserVo;
+
+
+
+@Controller
 public class BoardController {
 
 		BoardVo vo = new BoardVo();
+		UserVo vo1 = new UserVo();
 }
