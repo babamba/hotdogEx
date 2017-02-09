744e9d6b8207d48687c5d63f708c99129fba8280
bit-user <bit-user@bit>
2017-02-09 13:57:04 +0900

    .

--- src/main/java/com/hotdog/petcam/controller/BoardController.java
+++ src/main/java/com/hotdog/petcam/controller/BoardController.java
@@ -1,15 +1,22 @@
 package com.hotdog.petcam.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.hotdog.petcam.vo.BoardVo;
 import com.hotdog.petcam.vo.UserVo;
 
 
-
+@RequestMapping("/")
 @Controller
 public class BoardController {
 
 		BoardVo vo = new BoardVo();
 		UserVo vo1 = new UserVo();
+				
+		
+		public void test(){
+			System.out.println("ggg");
+		}
+		
 }
