a382435ce03c8c140c75a37d85594e85bf62ceb5
donghu <nap@nate.com>
2017-02-27 01:55:33 +0900

    main 접속할때 requestHeader로 웹뷰로 로그인 구현

--- src/main/java/com/hotdog/petcam/controller/MainController.java
+++ src/main/java/com/hotdog/petcam/controller/MainController.java
@@ -4,39 +4,60 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.hotdog.petcam.DTO.JSONResult;
 import com.hotdog.petcam.security.AuthUser;
 import com.hotdog.petcam.service.PetService;
+import com.hotdog.petcam.service.RaspberrypiService;
+import com.hotdog.petcam.service.UserService;
 import com.hotdog.petcam.vo.PetVo;
+import com.hotdog.petcam.vo.RaspberrypiVo;
 import com.hotdog.petcam.vo.UserVo;
 
 @Controller
 @RequestMapping("")
 public class MainController {
-	
+
 	@Autowired
 	private PetService petService;
-	
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private RaspberrypiService raspberrypiService;
+
 	@RequestMapping("/")
-	public String index(@AuthUser UserVo nickname, Model model, PetVo petVo) {
+	public String index(@RequestHeader(value = "email", defaultValue = "") String email,
+			@RequestHeader(value = "pass_word", defaultValue = "") String password,
+			@RequestHeader(value = "nickname", defaultValue = "") String nick, @AuthUser UserVo nickname, Model model,
+			PetVo petVo, HttpSession session) {
 		List<PetVo> list = petService.getHotdog(petVo);
 		model.addAttribute("list", list);
 		System.out.println("hotdog list!");
 		System.out.println("핫도그 메인에 뿌려질것들" + list);
 		
+		
+		// 앱 웹뷰에 로그인 구현
+		if (!"".equals(email) && !"".equals(password) && !"".equals(nick)) {
+			UserVo userVo = userService.login(email, password, nick);
+			RaspberrypiVo piVo = new RaspberrypiVo();
+			piVo.setUsers_no(userVo.getUsers_no());
+			piVo = raspberrypiService.selectByNo(piVo);
+			session.setAttribute("authUser", userVo);
+			session.setAttribute("piVo", piVo);
+		}
 		return "main/page_main";
 	}
-	
-	
+
 	@RequestMapping("/loginpage")
-	public String login(){
+	public String login() {
 		return "main/loginPage";
 	}
-
 
 }
